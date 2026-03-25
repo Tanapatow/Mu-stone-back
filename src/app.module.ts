4ad8b94fa3146adf5a27dd83f41715994024cac1
ThanawatTham <thanawat.sarinpong@gmail.com>
2026-03-25 16:53:33 +0700

    feat(common): Implement global transform interceptor and response decorator:

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from './config/config.module';
 import { DatabaseModule } from './database/database.module';
-import { APP_GUARD } from '@nestjs/core';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { AuthGuard } from './auth/guards/auth.guard';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { SecurityModule } from './shared/security/security.module';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 
 @Module({
   imports: [
@@ -16,6 +17,9 @@ import { SecurityModule } from './shared/security/security.module';
     SecurityModule,
   ],
   controllers: [],
-  providers: [{ provide: APP_GUARD, useClass: AuthGuard }],
+  providers: [
+    { provide: APP_GUARD, useClass: AuthGuard },
+    { provide: APP_INTERCEPTOR, useClass: TransformInterceptor },
+  ],
 })
 export class AppModule {}
---
+++ src/common/interceptors/transform.interceptor.ts
@@ -0,0 +1,40 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+
+import { Request } from 'express';
+import { map, Observable } from 'rxjs';
+import { SuccessResponse } from 'src/common/types/response.type';
+import { RESPONSE_MESSAGE_KEY } from '../decorators/message-response.decorator';
+import { Reflector } from '@nestjs/core';
+
+@Injectable()
+export class TransformInterceptor implements NestInterceptor {
+  constructor(private readonly reflector: Reflector) {}
+
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<SuccessResponse<unknown>> {
+    const request = context.switchToHttp().getRequest<Request>();
+    const path = request.url;
+
+    const message = this.reflector.getAllAndOverride<string | undefined>(
+      RESPONSE_MESSAGE_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+
+    return next.handle().pipe(
+      map((data: unknown) => ({
+        success: true,
+        message,
+        data,
+        path,
+        timestamp: new Date().toISOString(),
+      })),
+    );
+  }
+}
